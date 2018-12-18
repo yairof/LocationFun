5f1a8c170383f8ef5c2cc4e4406cbdb7d69f811d
Yairo Fernandez <yairo.fernandeznunez@facebook.com>
2018-12-18 17:58:53 -0500

    Initial Commit

---
+++ LocationFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationFun
+//
+//  Created by Yairo Fernandez on 12/18/18.
+//  Copyright © 2018 Yairo Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
