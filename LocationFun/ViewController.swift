8fc46ab0a6a53810117282609937fa07777ef4b5
Yairo Fernandez <yairologo@Yairos-MacBook-Pro.local>
2018-12-19 01:12:22 -0500

    first commit

--- LocationFun/ViewController.swift
+++ LocationFun/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet
+    var debugLabel: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        debugLabel.text = ""
+        locationManager.delegate = self
+        
     }
 
+    func addDebugText(_ text: String) {
+        debugLabel.text = "\(debugLabel.text ?? "")\n\(text)"
+    }
+    
+    @IBAction
+    func requestPermissionButtonPressed() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    @IBAction
+    func startUpdatingPressed() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    //: MARK - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let lastLocation = locations.last {
+            addDebugText("lat/lng: \(lastLocation.coordinate.latitude),\(lastLocation.coordinate.longitude)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            addDebugText("Access Granted")
+        case .notDetermined:
+            addDebugText("Access Denied")
+        case .restricted:
+            addDebugText("Access Denied")
+        case .denied:
+            addDebugText("Access Denied")
+        case .authorizedAlways:
+            addDebugText("Access Granted")
+        }
+    }
+    
 
 }
 
